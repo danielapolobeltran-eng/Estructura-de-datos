b9f63b92db7e67da0be79832bef8c9af2d6b181e
danielapolobeltran-eng <danielapolobeltran@gmail.com>
2026-08-28 13:53:46 -0500

    Create parametros.cs

---
+++ parametros.cs
@@ -0,0 +1,29 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class Program
+{
+    //procedimiento sumar o imprimir
+    public static void ImprimirSuma(int a, int b){ // a, b son los parametros
+         int suma = a + b;
+         Console.WriteLine(suma);   
+    }
+
+    public static void MostrarPotenciaX(double num1, double potencia){
+        double result = Math.Pow( num1, potencia);
+        Console.WriteLine($"La potencia de {num1} elevado a {potencia} es: {result}");
+    }
+
+    public static void Main(string[] args){
+        int x = 12, y = 3;
+
+        ImprimirSuma(y, x); //y, x (argumjentos actuales)
+        ImprimirSuma(1000, -1); //1000, -1 (argumentos literales)
+        MostrarPotenciaX(x,y);
+    }    
+
+    
+
+}
